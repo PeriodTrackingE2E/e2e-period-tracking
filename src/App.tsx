ebc4c906f80887117d398864a001041944dd1df3
Lorenzo Zaccagnini <lorenzozcg@gmail.com>
2022-07-02 17:29:18 +0200

    aes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { Component, createSignal } from "solid-js";
+import aes from "crypto-js/aes";
+import CryptoJS from "crypto-js";
+import logo from "./logo.svg";
+import styles from "./App.module.css";
 
 const App: Component = () => {
+  const [textData, setTextData] = createSignal("");
+  const [key, setKey] = createSignal("");
+  const [result, setResult] = createSignal("no result");
+  const [decryptResult, setDecryptResult] = createSignal("no result");
+
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <div class="container">
+        <h1>E2E Tracking app</h1>
+        <div class="row">
+          <div class="six columns">
+            <p>Insert key</p>
+            <input
+              type="text"
+              value={key()}
+              onChange={(e) => {
+                setKey(e.target.value);
+              }}
+            />
+            <p>Insert data</p>
+            <input
+              type="text"
+              value={textData()}
+              onChange={(e) => {
+                setTextData(e.target.value);
+              }}
+            />
+            <button
+              onClick={() => {
+                setResult(aes.encrypt(textData(), key()).toString());
+              }}
+            >
+              Encrypt
+            </button>
+            <div>{result()}</div>
+          </div>
+          <div class="six columns">
+            <p>Insert key for Decryption</p>
+            <input
+              type="text"
+              value={key()}
+              onChange={(e) => {
+                setKey(e.target.value);
+              }}
+            />
+            <button
+              onClick={() => {
+                let bytes = aes.decrypt(result(), key());
+                setDecryptResult(bytes.toString(CryptoJS.enc.Utf8));
+              }}
+            >
+              Decrypt
+            </button>
+            <p>{decryptResult()}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
